40363f59527499dd10f89d97b242ff3c029252c4
Alireza <alireza.ys245@gmail.com>
2024-12-24 12:32:33 +0330

    update 1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  router: {
+    base: '/Master-Movie/'
+  },
   compatibilityDate: '2024-11-01',
   ssr: false, 
   devtools: { enabled: false },
