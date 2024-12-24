9bc89d450cafe00252bfb248c1a7b7bbcdf302d8
Alireza <alireza.ys245@gmail.com>
2024-12-24 12:10:22 +0330

    publish pages

--- app.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="bg-gray-600 min-h-screen">
-    <NuxtPage class="bg-gray-600 min-h-screen" />
-    
-  </div>
-  
-</template>
--- plugins/bootstrap.js
+++
@@ -1,6 +0,0 @@
-import {Swiper, SwiperSlide} from 'swiper/vue';
-export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.component('Swiper', Swiper)
-    nuxtApp.vueApp.component('swiper-slide', SwiperSlide)
-
-});
